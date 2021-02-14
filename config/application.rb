3fa03eb73d2d35964000bb665095bdc7acc04ecd
富田慎一 <s.tomita@blue-corporation.jp>
2021-02-14 16:02:34 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,15 @@ module Baukis2
     config.i18n.load_path +=
       Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     config.i18n.default_locale = :ja
+
+    #今回はgeneratorsをrspec以外無効に
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
   end
 end
