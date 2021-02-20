5c39c09d15818ecf6ab2b54b27b8746efd107c38
富田慎一 <s.tomita@blue-corporation.jp>
2021-02-20 19:16:15 +0900

    403error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
-rescue_from StandardError, with: :rescue500
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
+
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -9,6 +15,11 @@ rescue_from StandardError, with: :rescue500
     else
       "customer"
     end
+  end
+
+  private def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
   end
 
   private def rescue500(e)
