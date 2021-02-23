bf46ddba8eb6c1106c9374b6390f750b2ff23d36
富田慎一 <s.tomita@blue-corporation.jp>
2021-02-23 09:52:03 +0900

    concern

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = false #開発用で一時無効化
-  # config.cache_classes = true
+  config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
