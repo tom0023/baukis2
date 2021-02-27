0795c904883c191ab63f0df908bf5c12da8f85d7
富田慎一 <s.tomita@blue-corporation.jp>
2021-02-27 17:39:37 +0900

    staff_authenticate

---
+++ spec/services/staff/authenticator_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+describe Staff::Authenticator do
+  describe "#authenticate" do
+    example "正しいパスワードなら true を返す" do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate("pw")).to be_truthy
+    end
+
+    example "誤ったパスワードなら false を返す" do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate("xy")).to be_falsey
+    end
+
+    example "パスワード未設定なら false を返す" do
+      m = build(:staff_member, password: nil)
+      expect(Staff::Authenticator.new(m).authenticate(nil)).to be_falsey
+    end
+
+    example "停止フラグが立っていれば false を返す" do
+      m = build(:staff_member, suspended: true)
+      expect(Staff::Authenticator.new(m).authenticate("pw")).to be_falsey
+    end
+
+    example "開始前なら false を返す" do
+      m = build(:staff_member, start_date: Date.tomorrow)
+      expect(Staff::Authenticator.new(m).authenticate("pw")).to be_falsey
+    end
+
+    example "終了時なら false を返す" do
+      m = build(:staff_member, end_date: Date.today)
+      expect(Staff::Authenticator.new(m).authenticate("pw")).to be_falsey
+    end
+  end
+end
+
+
