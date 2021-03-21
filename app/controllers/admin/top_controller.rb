cc859ed911af7bb409a7288965fd84fbeed43121
富田慎一 <s.tomita@blue-corporation.jp>
2021-03-21 16:16:00 +0900

    update 職員管理リンク設置

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,5 +1,10 @@
 class Admin::TopController < Admin::Base
   def index
-    render action: "index"
+    # render action: "index"
+    if current_administrator
+      render action: "dashboard"
+    else
+      render action: "index"
+    end
   end
 end
